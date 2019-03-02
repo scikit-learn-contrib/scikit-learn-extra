e39617378d0e6e4e6b9dd76282b3e544ad38432c
Roman Yurchak <rth.yurchak@pm.me>
2019-03-02 14:18:05 +0100

    Remove irrelevant parts of project template (#3)

--- sklearn_extra/tests/test_common.py
+++ sklearn_extra/tests/test_common.py
@@ -2,16 +2,12 @@ import pytest
 
 from sklearn.utils.estimator_checks import check_estimator
 
-from sklearn_extra import TemplateEstimator
-from sklearn_extra import TemplateClassifier
-from sklearn_extra import TemplateTransformer
 from sklearn_extra.kernel_approximation import Fastfood
 
 
 @pytest.mark.parametrize(
     "Estimator",
-    [TemplateEstimator, TemplateTransformer, TemplateClassifier,
-     Fastfood]
+    [Fastfood]
 )
 def test_all_estimators(Estimator, request):
     return check_estimator(Estimator)
