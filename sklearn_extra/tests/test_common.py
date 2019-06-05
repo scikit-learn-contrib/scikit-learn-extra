62a6375b1bf2b176df0fbbbb19038791d0b19548
Roman Yurchak <rth.yurchak@pm.me>
2019-06-05 14:27:29 +0200

    Lint with black

--- sklearn_extra/tests/test_common.py
+++ sklearn_extra/tests/test_common.py
@@ -5,9 +5,6 @@ from sklearn.utils.estimator_checks import check_estimator
 from sklearn_extra.kernel_approximation import Fastfood
 
 
-@pytest.mark.parametrize(
-    "Estimator",
-    [Fastfood]
-)
+@pytest.mark.parametrize("Estimator", [Fastfood])
 def test_all_estimators(Estimator, request):
     return check_estimator(Estimator)
