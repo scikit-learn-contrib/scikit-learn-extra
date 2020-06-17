10103f8932de4cc81ab7e27d8a27d0c92e1a1027
Roman Yurchak <rth.yurchak@gmail.com>
2020-06-17 22:15:38 +0200

    MNT scikit-learn 0.23 compatibility (#65)

--- sklearn_extra/tests/test_common.py
+++ sklearn_extra/tests/test_common.py
@@ -7,17 +7,15 @@ from sklearn_extra.cluster import KMedoids
 
 ALL_ESTIMATORS = [Fastfood, KMedoids, EigenProClassifier, EigenProRegressor]
 
-if hasattr(estimator_checks, "parametrize_with_checks"):
-    # Common tests are only run on scikit-learn 0.22+
 
-    @estimator_checks.parametrize_with_checks(ALL_ESTIMATORS)
-    def test_all_estimators(estimator, check, request):
-        # TODO: fix this common test failure cf #41
-        if isinstance(
-            estimator, EigenProClassifier
-        ) and "function check_classifier_multioutput" in str(check):
-            request.applymarker(
-                pytest.mark.xfail(run=False, reason="See issue #41")
-            )
+@estimator_checks.parametrize_with_checks([cls() for cls in ALL_ESTIMATORS])
+def test_all_estimators(estimator, check, request):
+    # TODO: fix this common test failure cf #41
+    if isinstance(
+        estimator, EigenProClassifier
+    ) and "function check_classifier_multioutput" in str(check):
+        request.applymarker(
+            pytest.mark.xfail(run=False, reason="See issue #41")
+        )
 
-        return check(estimator)
+    return check(estimator)
