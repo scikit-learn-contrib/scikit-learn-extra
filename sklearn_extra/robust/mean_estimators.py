8077c04eec2828a93cc18de873a0988bf9c03224
TimotheeMathieu <timothee.mathieu@u-psud.fr>
2021-06-25 10:58:48 +0200

    add c is None iqr heuristic and correct heuristic in robust_weighted_estimator

--- sklearn_extra/robust/mean_estimators.py
+++ sklearn_extra/robust/mean_estimators.py
@@ -88,7 +88,7 @@ def median_of_means(X, k, random_state=np.random.RandomState(42)):
     return median_of_means_blocked(x, blocks)[0]
 
 
-def huber(X, c=1.35, T=20, tol=1e-3):
+def huber(X, c=None, T=20, tol=1e-3):
     """Compute the Huber estimator of location of X with parameter c
 
     Parameters
@@ -97,10 +97,11 @@ def huber(X, c=1.35, T=20, tol=1e-3):
     X : array like, length = n_sample
         sample from which we want an estimator of the mean
 
-    c : float >0, default = 1.35
+    c : float >0, default = None
         parameter that control the robustness of the estimator.
         c going to zero gives a  behavior close to the median.
         c going to infinity gives a behavior close to sample mean.
+        if c is None, the iqr is used as heuristic.
 
     T : int, default = 20
         Number of iterations of the algorithm.
@@ -119,12 +120,17 @@ def huber(X, c=1.35, T=20, tol=1e-3):
     # Initialize the algorithm with a robust first-guess : the median.
     mu = np.median(x)
 
+    if c is None:
+        c_ = iqr(x)
+    else:
+        c_ = c
+
     def psisx(x, c):
         # Huber weight function.
         res = np.zeros(len(x))
-        mask = np.abs(x) <= c
+        mask = np.abs(x) <= c_
         res[mask] = 1
-        res[~mask] = c / np.abs(x[~mask])
+        res[~mask] = c_ / np.abs(x[~mask])
         return res
 
     # Create a list to keep the ten last values of mu
@@ -133,7 +139,7 @@ def huber(X, c=1.35, T=20, tol=1e-3):
     # Run the iterative reweighting algorithm to compute M-estimator.
     for t in range(T):
         # Compute the weights
-        w = psisx(x - mu, c)
+        w = psisx(x - mu, c_)
 
         # Infinite coordinates in x gives zero weight, we take them out.
         ind_pos = w > 0
