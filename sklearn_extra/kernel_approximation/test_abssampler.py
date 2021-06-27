c0e6ef31d58e374a6cae9fd3b1a350adb620479e
Gleb Levitskiy <36483986+GLevV@users.noreply.github.com>
2021-06-27 10:46:48 +0000

    Update test_abssampler.py

--- sklearn_extra/kernel_approximation/test_abssampler.py
+++ sklearn_extra/kernel_approximation/test_abssampler.py
@@ -10,7 +10,10 @@ X = rng.random_sample(size=(300, 50))
 
 def test_abss_expected_output_shape():
     N_COMPONENTS = 100
-    abss = AdaBoostStumpsSampler(n_components=N_COMPONENTS, random_state=rng)
+    abss = AdaBoostStumpsSampler(
+        n_components=N_COMPONENTS,
+        random_state=rng
+    )
     Xt = abss.fit_transform(X)
     assert X.shape[0] == Xt.shape[0]
     assert N_COMPONENTS == Xt.shape[1]
@@ -19,7 +22,10 @@ def test_abss_expected_output_shape():
 def test_abss_output_values():
     N_COMPONENTS = 100
     Xt_manual = np.zeros((X.shape[0], N_COMPONENTS))
-    abss = AdaBoostStumpsSampler(n_components=N_COMPONENTS, random_state=rng)
+    abss = AdaBoostStumpsSampler(
+        n_components=N_COMPONENTS,
+        random_state=rng
+    )
     Xt = abss.fit_transform(X)
     for col in range(N_COMPONENTS):
         Xt_manual[:, col] = np.sign(X[:, abss.random_columns_[col]] - abss.random_offsets_[col])
