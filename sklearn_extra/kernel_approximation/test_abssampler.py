8e40c0dd5260e1dd291a430988a7fc88bf86385c
Gleb Levitskiy <36483986+GLevV@users.noreply.github.com>
2021-06-27 10:10:48 +0000

    Create test_abssampler.py

---
+++ sklearn_extra/kernel_approximation/test_abssampler.py
@@ -0,0 +1,26 @@
+import pytest
+import numpy as np
+from numpy.testing import assert_array_almost_equal
+from sklearn_extra.kernel_approximation import AdaBoostStumpsSampler
+
+
+rng = np.random.RandomState(0)
+X = rng.random_sample(size=(300, 50))
+
+
+def test_abss_expected_output_shape():
+    N_COMPONENTS = 100
+    abss = AdaBoostStumpsSampler(n_components=N_COMPONENTS, random_state=rng)
+    Xt = abss.fit_transform(X)
+    assert X.shape[0] == Xt.shape[0]
+    assert N_COMPONENTS == Xt.shape[1]
+
+
+def test_abss_output_values():
+    N_COMPONENTS = 100
+    Xt_manual = np.zeros((X.shape[0], N_COMPONENTS))
+    abss = AdaBoostStumpsSampler(n_components=N_COMPONENTS, random_state=rng)
+    Xt = abss.fit_transform(X)
+    for col in range(N_COMPONENTS):
+        Xt_manual[:, col] = np.sign(X[:, abss.random_columns_[col]] - abss.random_offsets_[col])
+    assert_array_almost_equal(Xt_manual, Xt)
